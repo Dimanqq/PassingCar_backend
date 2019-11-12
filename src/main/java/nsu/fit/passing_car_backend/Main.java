99e3542795f42c69de25a3154c1a9b39457275f4
Ирина Федяева <i.fediaeva@g.nsu.ru>
2019-11-12 14:52:58 +0700

    Initial commit

---
+++ src/main/java/nsu/fit/passing_car_backend/Main.java
@@ -0,0 +1,16 @@
+package nsu.fit.passing_car_backend;
+
+import io.undertow.Undertow;
+import io.undertow.util.Headers;
+
+public class Main {
+    public static void main(String[] args) {
+        Undertow server = Undertow.builder().addHttpListener(8080,
+                "localhost").setHandler(exchange -> {
+            exchange.getResponseHeaders()
+                    .put(Headers.CONTENT_TYPE, "text/plain");
+            exchange.getResponseSender().send("Hello Baeldung");
+        }).build();
+        server.start();
+    }
+}
