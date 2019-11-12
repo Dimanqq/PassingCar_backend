ee14c158ad28e7a48270d403c7d4b4d4b5b00b86
Ирина Федяева <i.fediaeva@g.nsu.ru>
2019-11-12 16:04:41 +0700

    Test using undertow routers

--- src/main/java/nsu/fit/passing_car_backend/Main.java
+++ src/main/java/nsu/fit/passing_car_backend/Main.java
@@ -1,16 +1,33 @@
 package nsu.fit.passing_car_backend;
 
+import io.undertow.Handlers;
 import io.undertow.Undertow;
+import io.undertow.server.HttpHandler;
 import io.undertow.util.Headers;
 
 public class Main {
     public static void main(String[] args) {
+        HttpHandler h1 = Handlers.path()
+                .addPrefixPath("/route1", Handlers.path()
+                        .addPrefixPath("/branch1",
+                                exchange -> {
+                                    exchange.getResponseHeaders()
+                                            .put(Headers.CONTENT_TYPE, "text/plain");
+                                    exchange.getResponseSender().send("Hello from branch1");
+                                })
+                        .addPrefixPath("/branch2",
+                                exchange -> {
+                                    exchange.getResponseHeaders()
+                                            .put(Headers.CONTENT_TYPE, "text/plain");
+                                    exchange.getResponseSender().send("Hello from branch2");
+                                }))
+                .addPrefixPath("/route2", exchange -> {
+                    exchange.getResponseHeaders()
+                            .put(Headers.CONTENT_TYPE, "text/plain");
+                    exchange.getResponseSender().send("Hello from r2");
+                });
         Undertow server = Undertow.builder().addHttpListener(8080,
-                "localhost").setHandler(exchange -> {
-            exchange.getResponseHeaders()
-                    .put(Headers.CONTENT_TYPE, "text/plain");
-            exchange.getResponseSender().send("Hello Baeldung");
-        }).build();
+                "0.0.0.0").setHandler(h1).build();
         server.start();
     }
 }
