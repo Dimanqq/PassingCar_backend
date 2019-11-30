1425099ff6480562f13be84def05ddae263a1207
Deploy Server <chernik.nsk.ru@mail.ru>
2019-11-30 09:59:01 +0000

    Add git

--- run.sh
+++ run.sh
@@ -1,11 +1,16 @@
 #/bin/bash
 
 cd "${0%/*}"
+
+git checkout master
+git pull
+
 mvn package
 if [ $? != 0 ]
 then
 	exit -1
 fi
+
 sudo docker build -t app_i --no-cache .
 sudo docker rm -f app_w
 sudo docker run -d --name app_w -p 80:8080 app_i
