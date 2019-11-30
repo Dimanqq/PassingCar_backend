addb2b4876690c16ae4eec186ec55c25560d4adb
Chernik <chernik.nsk.ru@mail.ru>
2019-11-30 15:43:38 +0700

    Fix creds

--- src/main/java/nsu/fit/passing_car_backend/Main.java
+++ src/main/java/nsu/fit/passing_car_backend/Main.java
@@ -14,7 +14,7 @@ public class Main {
         final ServerUtils serverUtils;
         SQLCreds creds = new SQLCreds();
         Map<String, String> env = System.getenv();
-        creds.port = Integer.valueOf(env.getOrDefault("POSTGRES_PORT", "5432"));
+        creds.port = Integer.valueOf(env.getOrDefault("POSTGRES_PORT", "5433"));
         creds.ip = env.getOrDefault("POSTGRES_IP", "3.19.71.72");
         creds.db = env.getOrDefault("POSTGRES_DATABASE", "passing_car");
         creds.user = env.getOrDefault("POSTGRES_USER", "postgres");
