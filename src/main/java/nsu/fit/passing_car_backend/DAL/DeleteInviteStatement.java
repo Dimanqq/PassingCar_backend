bcd95b0707a7a736cfd27205485e20f4c5fe16b6
Chernik <chernik.nsk.ru@mail.ru>
2019-12-09 21:36:59 +0700

    Add remove invite

---
+++ src/main/java/nsu/fit/passing_car_backend/DAL/DeleteInviteStatement.java
@@ -0,0 +1,35 @@
+package nsu.fit.passing_car_backend.DAL;
+
+import nsu.fit.passing_car_backend.DataError;
+import nsu.fit.passing_car_backend.SQLStatement;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DeleteInviteStatement extends SQLStatement {
+    @Override
+    protected AssertMap getAssert() {
+        AssertMap map = new AssertMap();
+        map.put("user_id", String.class);
+        map.put("ride_id", String.class);
+        return map;
+    }
+
+    @Override
+    protected String getSQL() {
+        return "DELETE FROM m2m_ride_user " +
+                "WHERE user_id = ?::uuid " +
+                "AND ride_id = ?::uuid " +
+                "RETURNING user_id";
+    }
+
+    @Override
+    protected Map run(PreparedStatement statement, Map data) throws SQLException, DataError {
+        statement.setString(1, (String) data.get("user_id"));
+        statement.setString(2, (String) data.get("ride_id"));
+        try (ResultSet res = statement.executeQuery()) {
+            return Map.oneValue("status", res.next());
+        }
+    }
+}
--- src/main/java/nsu/fit/passing_car_backend/handlers/DeleteInviteHandler.java
+++ src/main/java/nsu/fit/passing_car_backend/handlers/DeleteInviteHandler.java
@@ -2,6 +2,10 @@ package nsu.fit.passing_car_backend.handlers;
 
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
+import nsu.fit.passing_car_backend.DAL.DeleteInviteStatement;
+import nsu.fit.passing_car_backend.DAL.GetRideStatement;
+import nsu.fit.passing_car_backend.DataError;
+import nsu.fit.passing_car_backend.SQLStatement;
 import nsu.fit.passing_car_backend.ServerUtils;
 
 public class DeleteInviteHandler implements HttpHandler {
@@ -12,7 +16,15 @@ public class DeleteInviteHandler implements HttpHandler {
     }
 
     @Override
-    public void handleRequest(HttpServerExchange httpServerExchange) throws Exception {
-        // TODO write
+    public void handleRequest(HttpServerExchange exchange) {
+        try{
+            SQLStatement.Map data = new SQLStatement.Map();
+            data.put("ride_id", exchange.getQueryParameters().get("id").getFirst());
+            data.put("user_id", exchange.getRequestHeaders().get("Authorization").getFirst());
+            data = serverUtils.sqlConnection.runStatement(data, new DeleteInviteStatement());
+            exchange.getResponseSender().send(data.toJSON().toString());
+        } catch (DataError e){
+            e.send(exchange);
+        }
     }
 }
