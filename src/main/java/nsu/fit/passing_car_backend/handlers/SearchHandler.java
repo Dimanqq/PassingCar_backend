b4575a01f143d9c473a5b03e42607400dfaf1b95
Chernik <chernik.nsk.ru@mail.ru>
2019-12-10 00:55:42 +0700

    Add search ride

--- src/main/java/nsu/fit/passing_car_backend/handlers/SearchHandler.java
+++ src/main/java/nsu/fit/passing_car_backend/handlers/SearchHandler.java
@@ -2,6 +2,8 @@ package nsu.fit.passing_car_backend.handlers;
 
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
+import nsu.fit.passing_car_backend.DAL.SearchStatement;
+import nsu.fit.passing_car_backend.DataError;
 import nsu.fit.passing_car_backend.SQLStatement;
 import nsu.fit.passing_car_backend.ServerUtils;
 
@@ -14,9 +16,14 @@ public class SearchHandler implements HttpHandler {
 
     @Override
     public void handleRequest(HttpServerExchange exchange) {
-        SQLStatement.Map data = SQLStatement.Map.fromExchangeQuery(exchange);
-        data.put("user_id", exchange.getRequestHeaders().get("Authorization").getFirst());
-        exchange.setStatusCode(200);
-        exchange.getResponseSender().send(data.toJSON().toString());
+        try {
+            SQLStatement.Map data = SQLStatement.Map.fromExchangeQuery(exchange);
+            data.put("user_id", exchange.getRequestHeaders().get("Authorization").getFirst());
+            exchange.setStatusCode(200);
+            data = serverUtils.sqlConnection.runStatement(data, new SearchStatement());
+            exchange.getResponseSender().send(data.toJSON().toString());
+        } catch (DataError e) {
+            e.send(exchange);
+        }
     }
 }
