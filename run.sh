d5f16d4d6c8e33c5cab2345e2362cb45a8cb5707
Chernik <chernik.nsk.ru@mail.ru>
2019-12-18 01:15:14 +0700

    Fix shell script

--- run.sh
+++ run.sh
@@ -5,16 +5,16 @@ cd "${0%/*}"
 git checkout master
 git pull
 
-mvn sonar:sonar \
-	-Dsonar.projectKey=PA \
-	-Dsonar.host.url=http://84.237.50.237:9000 \
-	-Dsonar.login=44e6579881d8e05a512f899fc911d5937d7e2fe6 # under Chernik
-
 mvn package
 if [ $? != 0 ]
 then
 	exit -1
 fi
+
+mvn sonar:sonar \
+	-Dsonar.projectKey=PA \
+	-Dsonar.host.url=http://84.237.50.237:9000 \
+	-Dsonar.login=44e6579881d8e05a512f899fc911d5937d7e2fe6 # under Chernik
 
 sudo docker build -t app_i --no-cache .
 sudo docker rm -f app_w
